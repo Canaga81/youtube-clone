d73806e70a2838ffe426911da2c0469bad87506f
Canaga81 <Canagamemmedov@gmail.com>
2024-04-28 14:50:33 +0400

    incard section html structure finish

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,11 +9,92 @@ import jack from '../../assets/jack.png';
 import user_profile from '../../assets/user_profile.jpg';
 
 const PlayVideo = () => {
+
   return (
+
     <div className='play-video'>
+
         <video src={video1} controls autoPlay muted></video>
+        <h3>En gozel web sayt tutorial videosu</h3>
+        <div className='play-video-info'>
+            <p>18k views &bull; 9 days ago</p>
+            <div>
+                <span><img src={like} alt="" />3k</span>
+                <span><img src={dislike} alt="" />96</span>
+                <span><img src={share} alt="" />Share</span>
+                <span><img src={save} alt="" />Save</span>
+            </div>
+        </div>
+        <hr />
+
+        <div className="publisher">
+            <img src={jack} alt="" />
+            <div>
+                <p>Canaga</p>
+                <span>1M Subscribers</span>
+            </div>
+            <button>Subscribe</button>
+        </div>
+
+        <div className='vid-description'>
+            <p>Channel that makes learning Easy</p>
+            <p>Subscribe GreatStack to Watch More Tutorials on web development</p>
+            <hr />
+            <h4>150 Comments</h4>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Bayram Musayev <span>1 day ago</span></h3>
+                    <p>Herkese salam olsun. Bu video ela videodur.</p>
+                    <div className='comment-action'>
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Bayram Musayev <span>1 day ago</span></h3>
+                    <p>Herkese salam olsun. Bu video ela videodur.</p>
+                    <div className='comment-action'>
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Bayram Musayev <span>1 day ago</span></h3>
+                    <p>Herkese salam olsun. Bu video ela videodur.</p>
+                    <div className='comment-action'>
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Bayram Musayev <span>1 day ago</span></h3>
+                    <p>Herkese salam olsun. Bu video ela videodur.</p>
+                    <div className='comment-action'>
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-  )
+
+  );
+
 }
 
 export default PlayVideo;
